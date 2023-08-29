56b316f4a8611bb6726760a88e87d47f88e57acb
VictorVerdadeiro <143470425+VictorVerdadeiro@users.noreply.github.com>
2023-08-29 00:30:20 -0300

    Mudei a mensagem a ser enviada

--- src/app.js
+++ src/app.js
@@ -12,6 +12,6 @@ yesBtn.addEventListener("click", function() {
   noBtn.classList.add("hidden");
   loveMessage.classList.remove("hidden");
   setTimeout(() => {
-    window.location.href = "https://api.whatsapp.com/send?l=pt-BR&phone=5548998443415&text=eu%20te%20amo%2C%20Victor%2C%20voc%C3%AA%20%C3%A9%20o%20meu%20tudo.%20Aceito%20me%20casar%20com%20voc%C3%AA%F0%9F%92%8D%F0%9F%92%9E";
+    window.location.href = "https://api.whatsapp.com/send?l=pt-BR&phone=5548998443415&text=Sim%2C%20Victor%2C%20eu%20aceito%20me%20casar%20com%20voc%C3%AA.%20Voc%C3%AA%20%C3%A9%20o%20verdadeiro%20victor%20e%20o%20verdadeiro%20dono%20do%20meu%20cora%C3%A7%C3%A3o.";
   }, 3500);
 });
